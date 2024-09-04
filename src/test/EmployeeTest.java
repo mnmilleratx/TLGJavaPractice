0f40faad81585b68e905013e48334430c387b321
Megan Miller <MeganM@tlgcohort.com>
2024-09-04 00:09:33 -0500

    added unit testing code in EmployeeTest

--- src/test/EmployeeTest.java
+++ src/test/EmployeeTest.java
@@ -1,11 +1,22 @@
+// src/test/java/com/hr/personnel/EmployeeTest.java
 package com.hr.personnel;
 
 import org.junit.Test;
+
+import java.time.LocalDate;
+
 import static org.junit.Assert.assertEquals;
 
 public class EmployeeTest {
     @Test
     public void testGetEmployeeInfo() {
-        Employee emp = new Employee("Micah",)
+        Employee emp = new Employee("Mike", LocalDate.of(2020, 2, 3));
+        assertEquals("name = Mike, hireDate = 2020-02-03", emp.getEmployeeInfo());
+    }
+
+    @Test
+    public void testWork() {
+        Employee emp = new Employee("Mike", LocalDate.of(2020, 2, 3));
+        assertEquals("Mike worked", emp.work());
     }
 }
